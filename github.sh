5a00c64a826fd0194605adfbc66a7a09475486e4
luoran852 <hannn0414@naver.com>
2020-06-21 22:02:17 +0900

    xx commit

--- github.sh
+++ github.sh
@@ -1,6 +1,7 @@
 git config --global user.email hannn0414@naver.com
 git config --global user.name luoran852
 
+# 초기 생성 
 git init
 
 git add .
@@ -10,3 +11,10 @@ git remote add origin https://github.com/luoran852/jungol_Python.git
 
 git push -u origin master
 
+# 이후 업데이트
+git add .
+git commit -m "xx commit"
+git push
+
+# 저장소 -> 로컬
+git pull
